18b6d6e2cc562e75cb384d8f335003138a0a16ca
Fedir Derhachov <fdeordergachev@gmail.com>
2025-04-12 03:25:30 +0200

    feat:first shader created

--- app/components/Model.tsx
+++ app/components/Model.tsx
@@ -1,12 +1,24 @@
-import React from 'react';
+import React, { useMemo } from 'react';
 import { fragment, vertex } from '../shaders/gradient-shader';
+import { useThree } from '@react-three/fiber';
+import * as THREE from 'three';
 
 
 const Model = () => {
+   // const { size } = useThree();
+   const size = new THREE.Vector3(5, 5, 1); // plane size (width, height)
+
+   const uniforms = useMemo(() => ({
+      iResolution: { value: size },
+   }), []);
+   // const uniforms = useMemo(() => ({
+   //    iResolution: { value: new THREE.Vector3(size.width, size.height, 1.0) },
+   // }), [size.width, size.height]);
    return (
       <mesh>
          <planeGeometry args={[5, 5, 300, 300]} />
          <shaderMaterial
+            uniforms={uniforms}
             fragmentShader={fragment}
             vertexShader={vertex}
          />
--- app/shaders/gradient-shader.js
+++ app/shaders/gradient-shader.js
@@ -1,10 +1,13 @@
 export const vertex = /* glsl */ `
+   varying vec2 vUv;
    void main() {
+      vUv = uv;
       gl_Position = projectionMatrix * modelViewMatrix * vec4(position, 1.0);
    }
 `;
 
 export const fragment = /* glsl */`
+   uniform vec3 iResolution;
    uniform float time;
    varying vec2 vUv;
    varying vec3 vPosition;
@@ -12,6 +15,7 @@ export const fragment = /* glsl */`
    float PI = 3.141592653589793238;
    
    void main(){
-      gl_FragColor = vec4(1.0, 0.0, 0.0, 1.0);
+      vec2 uv = vUv; 
+      gl_FragColor = vec4(uv, 0.0, 1.0);
    }
 `
