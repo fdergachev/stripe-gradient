ac9530852e3796cb0109950cb0fcf361c9101426
Fedir Derhachov <fdeordergachev@gmail.com>
2025-04-12 11:57:56 +0200

    feat:shader-starter

--- app/components/Model.tsx
+++ app/components/Model.tsx
@@ -6,21 +6,19 @@ import * as THREE from 'three';
 
 const Model = () => {
    // const { size } = useThree();
-   const size = new THREE.Vector3(5, 5, 1); // plane size (width, height)
 
-   const uniforms = useMemo(() => ({
-      iResolution: { value: size },
-   }), []);
    // const uniforms = useMemo(() => ({
    //    iResolution: { value: new THREE.Vector3(size.width, size.height, 1.0) },
    // }), [size.width, size.height]);
    return (
       <mesh>
-         <planeGeometry args={[5, 5, 300, 300]} />
+         {/* args={[5, 5, 300, 300]} */}
+         <planeGeometry args={[5, 5, 50, 50]} />
          <shaderMaterial
-            uniforms={uniforms}
+            // uniforms={uniforms}
             fragmentShader={fragment}
             vertexShader={vertex}
+            wireframe={true}
          />
          {/* <meshBasicMaterial color={"red"} wireframe={true} /> */}
       </mesh>
--- app/shaders/gradient-shader.js
+++ app/shaders/gradient-shader.js
@@ -1,15 +1,20 @@
 export const vertex = /* glsl */ `
+
    varying vec2 vUv;
+   varying vec3 vColor
+
+
    void main() {
       vUv = uv;
       gl_Position = projectionMatrix * modelViewMatrix * vec4(position, 1.0);
    }
+
 `;
 
 export const fragment = /* glsl */`
-   uniform vec3 iResolution;
    uniform float time;
    varying vec2 vUv;
+   varying vec3 vColor
    varying vec3 vPosition;
    uniform vec2 pixels;
    float PI = 3.141592653589793238;
